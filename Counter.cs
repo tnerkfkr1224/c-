638ce6a85de39a7eea3b68a68f79e4c6eaa2266f
tnerkfkr1224 <148286428+tnerkfkr1224@users.noreply.github.com>
2023-10-18 08:41:19 +1100

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Program
+{
+    public class Counter
+    {
+        private int _count;
+        private string _name;
+
+        public Counter(string name)
+        {
+            _name = name;
+            _count = 0;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Reset()
+        {
+            _count = 0;
+        }
+
+        public int Ticks
+        {
+            get
+            {
+                return _count;
+            }
+        }
+    }
+}
+
